b44b92864056533e466a442b887ea385c32b1407
Nimesh Patel <nimeshpatel@Nimeshs-MacBook-Pro.local>
2023-05-28 14:33:11 +0530

    1st commit

--- exercises/exercise3/main.tf
+++ exercises/exercise3/main.tf
@@ -158,7 +158,7 @@ resource "aws_security_group" "bastion" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.workstation_ip]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
